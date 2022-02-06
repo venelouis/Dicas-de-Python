7a5be941a613260d0d8f1dcec611231145e84934
Venelouis <37526748+venelouis@users.noreply.github.com>
2022-02-06 15:13:20 -0300

    Update Melhor jeito de "printar"?.py
    adicionando pulo de linhas e etc

--- Melhor jeito de "printar"?.py
+++ Melhor jeito de "printar"?.py
@@ -1,6 +1,10 @@
 print("""Existe um jeito de printar no Python que é "melhor" que outro?
 
-Depende do que você considera "melhor", se for "printar" algo mantendo a formatação colocada, então, utilizando 3 pares de aspas duplas ou simples 
+Depende do que você considera "melhor", 
+
+se for "printar" algo mantendo a formatação colocada, 
+
+então utilizando 3 pares de aspas duplas ou simples 
 no inicio de cada print() vai manter toda a formatação como:
 
 - pular linhas, 
